047756fe399ba4cfa5f6a4cb4499a21fe97c49ca
Avi Das <adas@colgate.edu>
2013-07-27 10:30:38 -0400

    add post

--- venues_server.py
+++ venues_server.py
@@ -18,21 +18,6 @@ app = Flask(__name__)
 # Change to venues, which have autoincremented id, lat, lon, (/devsnearme/api/v0.1/venues?latitude=43.23432&longitude=-73.234234&radius)
 # min_turnout, expected_turnout, max_turnout
 
-results = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
-
 venues = [
   {
      "geometry" : {
@@ -124,20 +109,22 @@ def get_venue(venue_id):
 def not_found(error):
     return make_response(jsonify( { 'error': 'Not found' } ), 404)
 
-'''
+
 @app.route('/devsnearme/api/v0.1/venues', methods = ['POST'])
 def create_venue():
-    if not request.json or not 'title' in request.json:
+    if not request.json or not 'name' in request.json:
         abort(400)
     venue = {
         'id': venues[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
+        'name': request.json['name'],
+        'expected_attendance': request.json.get('expected_attendance', 0),
+        'min_attendance': request.json.get('min_attendance', 0),
+        'max_attendance': request.json.get('max_attendance', 0),
         'done': False
     }
     venues.append(venue)
     return jsonify( { 'venue': venue } ), 201
-'''
+
 
 @app.route('/devsnearme/api/v0.1/venues/<int:venue_id>', methods = ['DELETE'])
 def delete_venue(venue_id):
@@ -146,24 +133,6 @@ def delete_venue(venue_id):
         abort(404)
     venues.remove(venue[0])
     return jsonify( { 'result': True } )
-'''
-@app.route('/devsnearme/api/v0.1/venues/<int:venue_id>', methods = ['PUT'])
-def update_venue(venue_id):
-    venue = filter(lambda t: t['id'] == venue_id, venues)
-    if len(venue) == 0:
-        abort(404)
-    if not request.json:
-        abort(400)
-    if 'title' in request.json and type(request.json['title']) != unicode:
-        abort(400)
-    if 'description' in request.json and type(request.json['description']) is not unicode:
-        abort(400)
-    if 'done' in request.json and type(request.json['done']) is not bool:
-        abort(400)
-    venue[0]['title'] = request.json.get('title', venue[0]['title'])
-    venue[0]['description'] = request.json.get('description', venue[0]['description'])
-    venue[0]['done'] = request.json.get('done', venue[0]['done'])
-    return jsonify( { 'venue': venue[0] } )
-'''
+
 if __name__ == '__main__':
     app.run(debug = True)
